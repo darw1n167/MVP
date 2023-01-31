546a210ff5d8fcb87f75ffea35d1fac1ccdf3de4
Darwin Facelo <darwinfacelo@yahoo.com>
2023-01-31 08:27:36 -0800

    started deploying

--- front-end/app.js
+++ front-end/app.js
@@ -1,18 +1,23 @@
-// let URL = 'https://macro-tracker.onrender.com/'
-
-// const listedFood = () => {
-//   fetch (URL + "food")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data)
-    
-//   })
-
-// }
 let $food = $('<div class="food"></div>');
-let home = document.querySelector('#home')
-let entries = document.querySelector('#entries')
-let $entries = $('#entries')
+let home = document.querySelector('#home');
+let entries = document.querySelector('#entries');
+let $entries = $('#entries');
+let $tableBody = $(`#tableBody`);
+let tableBody = document.getElementById('tableBody');
+// let table = $(
+// `<table class="table">
+//   <thead>
+//     <tr>
+//       <th scope="col">Name</th>
+//       <th scope="col">Carbs</th>
+//       <th scope="col">Fats</th>
+//       <th scope="col">Proteins</th>
+//       <th scope="col">Calories</th>
+//     </tr>
+//   </thead>
+//   <tbody>`);
+// $entries.append(table);
+
 
 const loadFood = async () => {
   await fetch ('http://localhost:8000/food')
@@ -20,9 +25,25 @@ const loadFood = async () => {
   .then((data) => {
     data.forEach((element) => {
       console.log(element)
-      let div = document.createElement('div');
-      div.innerText = element.food_name;
-     $entries.append(div)
+    //   let  = $(`<div id="foodColName" class="row row-cols-5">
+    //   <div class="col">Name</div>
+    //   <div class="col">Carbs</div>
+    //   <div class="col">Fats</div>
+    //   <div class="col">Protein</div>
+    //   <div class="col">Calories</div>`)
+    //  $entries.append(div)
+    // let foodInfo = $(`<div id="food"> ${element.food_name}</div>`);
+    // entries.append(foodInfo)
+    let tBody = $(
+    `<tr>
+      <th scope="row">${element.food_name}</th>
+      <td>${element.carbs}</td>
+      <td>${element.fats}</td>
+      <td>${element.protein}</td>
+      <td>${element.calories}</td>
+    </tr>`);
+
+    $tableBody.append(tBody)
     });
   });
 }
@@ -31,7 +52,6 @@ const loadFood = async () => {
 const populate = (data) => {
 
 }
-// loadFood();
+loadFood();
 
 home.addEventListener('click', loadFood)
-
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();

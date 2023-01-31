7afc651700ca205fc64b0f47dc1d5019e0609f0f
Darwin Facelo <darwinfacelo@yahoo.com>
2023-01-30 17:49:38 -0800

    Added fetch to get information from DB

--- front-end/app.js
+++ front-end/app.js
@@ -11,3 +11,26 @@
 // }
 
 let $food = $('<div class="food"></div>');
+let home = document.querySelector('#home')
+
+
+const loadFood = async () => {
+  await fetch ('http://localhost:8000/food')
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((element) => {
+      console.log(element)
+      let {food_name} = element;
+      console.log(element.id)
+      console.log(food_name)
+    });
+  });
+}
+console.log
+
+const populate = (data) => {
+
+}
+// loadFood();
+
+home.addEventListener('click', loadFood)

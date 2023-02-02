6ccaff3980540c272c0d848d2aaa8a669f3c460a
Darwin Facelo <darwinfacelo@yahoo.com>
2023-02-01 19:10:48 -0800

    Completed Delete functionality working on Update functionality

--- public/app.js
+++ public/app.js
@@ -14,52 +14,88 @@ let $carb = $('#carb');
 let $fat = $('#fat')
 let $protein = $('#protein')
 let $calorie = $('#calorie')
-
-
-// const clearText = () => {
-//   let $typein = $('#typeIn');
-//   let $carb = $('#carb');
-//   let $fat = $('#fat');
-//   let $protein = $('#protein');
-//   let $calorie = $('#calorie');
-
-//   if ($typein.val() && $carb.val() && $fat.val() && $protein.val() && $calorie.val() != "")  {
-//     $typein.val() = '';
-//     $carb.val() = '';
-//     $fat.val() = '';
-//     $protein.val() = '';
-//     $calorie.val() = '';
-//   }
-// }
-
+let changeButtons = document.querySelectorAll('.change');
+let inputDisplay = false;
 
 const loadFood = async () => {
-  await fetch ('/food')
+  await fetch ('http://localhost:8000/food')
   .then((response) => response.json())
   .then((data) => {
     data.forEach((element) => {
       console.log(element)
     let tBody = $(
     `<tr>
-      <th scope="row">${element.food_name}</th>
-      <td class="entry">${element.carbs}</td>
-      <td class="entry">${element.fats}</td>
-      <td class="entry">${element.protein}</td>
-      <td class="entry">${element.calories}</td>
+      <th class="entry" id="food-${element.id}">${element.food_name}</th>
+      <td class="entry" id="carbs-${element.id}">${element.carbs}</td>
+      <td class="entry" id="fats-${element.id}">${element.fats}</td>
+      <td class="entry" id="protein-${element.id}">${element.protein}</td>
+      <td class="entry" id="calories-${element.id}">${element.calories}</td>
       <td class="buttons">
       <div class="dropdown">
-        <button class="edit">Edit</button>
       <button class="delete" id="${element.id}">Delete</button>
+      <button class="update" id="${element.id}">Update</button>
+
       </div>
       </td>
     </tr>`);
 
 
     $tableBody.append(tBody);
+
+    // $('.edit').click(function() {
+    //   $(this).closest('tr').find('.entry').each(function() {
+    //     let currentValue = $(this).text();
+    //     $(this).html(`<input type="text" value="${currentValue}">`);
+    //   });
     
+      $('.update').on('click', async function(e)  {
+        e.preventDefault();
+        let food = $(`#food-${element.id}`)
+        let {id} = this;
+        console.log(id)
+        fetch(`http://localhost:8000/food/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+        },
+          body: JSON.stringify({
+            "food_name": `${$typein.val()}`,
+            "carbs": `${$carb.val()}`,
+            "fats": `${$fat.val()}`,
+            "protein": `${$protein.val()}`,
+            "calories": `${$calorie.val()}`
+          })
+        }).then((res)=> {
+            return res.json();
+          }).then((data)=> {
+            // console.log(data.id);
+            console.log('Changed')
+            tBody.remove();
+            // let updateBody = $(`<tr>
+            // <th class="entry">${$typein.val()}</th>
+            // <td class="entry">${$carb.val()}</td>
+            // <td class="entry">${$fat.val()}</td>
+            // <td class="entry">${$protein.val()}</td>
+            // <td class="entry">${$calorie.val()}</td>
+            // <td class="buttons">
+            // <div class="dropdown">
+            // <button class="delete" id="${element.id}">Delete</button>
+            // <button class="update" id="${element.id}">Update</button>
+            // </div>
+            // </td>
+            // </tr>`);
+            // $tableBody.append(updateBody)
+          })
+        })
+
+      
+
+
     $(`.delete`).on("click", async function(e) {
+      e.preventDefault();
       const {id} = this;
-       fetch(`/food/${id}`, {
+      console.log(id)
+       fetch(`http://localhost:8000/food/${id}`, {
         method: 'DELETE'
       }).then((response)=> {
         if(response.ok)  {
@@ -67,11 +103,32 @@ const loadFood = async () => {
         } else {
           console.error('Failed to delete')
         }
-
       })
+      tBody.remove();
     });
-   
 
+
+    $(`#btn-${element.id}`).on('click', async function(e) {
+      e.preventDefault();
+      console.log('Clicked')
+      let entry = document.querySelectorAll('.entry')
+      if (!inputDisplay)  {
+        entry.forEach((td) => {
+          td.innerHTML = `<input type="text" value=${td.textContent}>`; 
+        });
+        inputDisplay = true;
+      } else {
+        entry.forEach((td) => {
+          td.textContent = td.firstElementChild.value;
+          console.log(td.textContent)
+        })
+        inputDisplay = false;
+      }
+    })
+      
+      // $(`btn-${element.id}`).click((e) => {
+      //   $(`food-${element.id}`).html
+      // })
 
     });
   });
@@ -80,15 +137,10 @@ const loadFood = async () => {
 
 
 
-
-const populate = (data) => {
-
-}
-
-
 $add.on('click', async (e) => {
+  e.preventDefault();
   try {     
-    const response = await fetch('/food', {
+    const response = await fetch('http://localhost:8000/food', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -113,12 +165,13 @@ $add.on('click', async (e) => {
       <td class="entry">${data.calories}</td>
       <td class="buttons">
       <div class="dropdown">
-        <button class="edit">Edit</button>
-      <button class="delete">Delete</button>
+        <button class="delete" id="${data.id.id}">Delete</button>
+        <button class="update" id="${data.id.id}">Update</button>
       </div>
       </td>
     </tr>`);
     $tableBody.append(newInfo);
+    deleteListener();   
     })
     console.log('Completed!');
   } catch(err) {
@@ -127,3 +180,21 @@ $add.on('click', async (e) => {
 })
 
 loadFood();
+
+
+let deleteListener = function() {  $(`.delete`).on("click", async function(e) {
+  e.preventDefault();
+  const {id} = this;
+  console.log(id)
+   fetch(`http://localhost:8000/food/${id}`, {
+    method: 'DELETE'
+  }).then((response)=> {
+    if(response.ok)  {
+      console.log('Data was deleted')
+    } else {
+      console.error('Failed to delete')
+    }
+  })
+  newInfo.remove();
+});
+}
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const pool = new Pool ({
         // port: 5432,
         // password: '',
         // database: 'food',
-        connectionString: process.env.URL_DATABASE
+        connectionString: process.env.DATABASE_URL
 });
 
 pool.connect();
@@ -51,7 +51,7 @@ app.put('/food/:id', async(req,res)=> {
         const {id} = req.params; 
         const {food_name, carbs, fats, protein, calories} = req.body;
         await pool.query('UPDATE food SET food_name = $1, carbs = $2, fats = $3, protein = $4, calories = $5 WHERE id = $6', [food_name, carbs, fats, protein, calories, id]);
-        res.json('Updated food where ID is ' + id)
+        res.json(JSON.parse(id))
     } catch (error) {
         console.error(error.message)
     }
@@ -73,8 +73,9 @@ app.post('/food', async(req,res) => {
     try {
         const {carbs, fats, protein, calories} = req.body;
         const foodName = req.body.food_name
-         const {rows} = await pool.query('INSERT INTO food (food_name, carbs, fats, protein, calories) VALUES ($1, $2, $3, $4, $5)', [foodName, carbs, fats, protein, calories])
-        res.send(req.body)
+        const {rows} = await pool.query('INSERT INTO food (food_name, carbs, fats, protein, calories) VALUES ($1, $2, $3, $4, $5) RETURNING id', [foodName, carbs, fats, protein, calories])
+        const id = rows[0];
+        res.send({...req.body, id})
     } catch (error) {
         console.error(error)
     }
